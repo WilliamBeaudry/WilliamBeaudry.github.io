e4c74ab9708d76d2690299e2c1956687119f9fcf
william.beaudry5@gmail.com <william.beaudry5@gmail.com>
2024-11-22 16:40:15 -0500

    ajouter une deuxième visualisation

--- projet_session/visualisation_dt.R
+++ projet_session/visualisation_dt.R
@@ -6,12 +6,23 @@ library(tidyr)
 df_long <- df_cln %>%
   pivot_longer(cols = -"Province/territoire", names_to = "Année", values_to = "Taux_participation")
 
-# Définir une palette de couleurs personnalisée
-couleurs <- c("Québec" = "blue", 
-              setNames(scales::hue_pal()(length(unique(df_long$"Province/territoire")) - 1), 
-                       setdiff(unique(df_long$"Province/territoire"), "Québec")))
+# Graphique linéaire
+ggplot(df_long, aes(x = Année, y = Taux_participation, group = `Province/territoire`, color = `Province/territoire`)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  labs(
+    title = "Taux de participation aux élections fédérales par province (2004-2011)",
+    x = "Année", y = "Taux de participation (%)",
+    color = "Province"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "right"
+  ) +
+  scale_color_manual(values = couleurs)
 
-# Créer le graphique
+# Créer un graphique boxplot
 ggplot(df_long, aes(x = Année, y = Taux_participation, group = "Province/territoire", color = "Province/territoire")) +
   geom_line() +
   geom_point() +
@@ -23,18 +34,19 @@ ggplot(df_long, aes(x = Année, y = Taux_participation, group = "Province/territ
         legend.position = "right") +
   scale_color_manual(values = "red")
 
-#Créer un graphique boxplot
-ggplot(df_long, aes(x = Année, y = Taux_participation)) +
-  geom_boxplot() +
-  geom_point(data = df_long %>% filter("Province/territoire" == "Québec"),
-             aes(color = "Québec"),
-             size = 3) +
-  scale_color_manual(values = c("Québec" = "red")) +
-  theme_minimal() +
-  labs(title = "Distribution des taux de participation par année",
-       subtitle = "Points rouges représentent le Québec",
-       x = "Année",
-       y = "Taux de participation (%)",
-       color = "Province/territoire") +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# Code pour un line plot
+# Palette de couleurs personnalisée
+couleurs <- c("Québec" = "blue",
+              setNames(scales::hue_pal()(length(unique(df_long$`Province/territoire`)) - 1),
+                       setdiff(unique(df_long$`Province/territoire`), "Québec")))
+
+
+
+
+
+
+
+
+
+
   
